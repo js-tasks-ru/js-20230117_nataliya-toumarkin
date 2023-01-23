498b964316db74d5d05b9c609617608e4cad4597
Nataliya Toumarkin <toumarkinn@gmail.com>
2023-01-23 14:07:05 +0200

    02-solve

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,24 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+const arr = [
+  "Детский велосипед Lexus Trike Racer Trike",
+  "Соска (пустышка) NUK 10729357",
+  "Соска (пустышка) Philips SCF182/12",
+  "ТВ тюнер D-COLOR  DC1301HD",
+  "Powerbank аккумулятор Hiper SP20000",
+];
+
+export function sortStrings(arr, param = "asc") {
+  const arrSort = arr
+    .slice()
+    .sort((a, b) => a.localeCompare(b, "ru", { caseFirst: "upper" }));
+  if (param === "asc") {
+    return arrSort;
+  } else {
+    return arrSort.reverse();
+  }
 }
+const sorted = sortStrings(arr, "desc");
+console.log(sorted);
