a0e8eeeeeb6ea4ec5756eae1cd07e4eac83b353d
Nataliya Toumarkin <toumarkinn@gmail.com>
2023-01-24 07:58:19 +0200

    2-pick solve

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -4,6 +4,15 @@
  * @param {...string} fields - the properties paths to pick
  * @returns {object} - returns the new object
  */
-export const pick = (obj, ...fields) => {
 
+const obj = {};
+
+export const pick = (obj, ...fields) => {
+  const res = {};
+  Object.entries(obj).forEach(([key, value]) => {
+    if (fields.includes(key)) {
+      res[key] = value;
+    }
+  });
+  return res;
 };
